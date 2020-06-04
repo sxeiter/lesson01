deca655b34804a14a6a7f3333abc4d4316a6b64a
Pavel Baksheev <sxeiter@mail.ru>
2020-06-04 21:53:11 +0500

    начало моего проекта

---
+++ main.js
@@ -0,0 +1,14 @@
+let money; 
+let income; 
+let addExpenses; 
+let deposit; 
+let mission; 
+let period;
+
+setTimeout(reminder, 3000);
+function reminder() {
+    alert("Так и будешь смотреть на эту скучную страницу?");
+}
+
+
+console.log('Посмотри вверх =) ');
