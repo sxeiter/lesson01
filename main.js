9f2b7e8ba5c08ff29cdced4714725cb1ca2068ae
Pavel Baksheev <sxeiter@mail.ru>
2020-06-08 18:38:34 +0500

    выполнил дз №3

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
-let money = 40000; 
+let money = prompt('Ваш ежемесячный доход?'); 
 let income = 'продаю мёд'; 
-let addExpenses = 'Еда, Коммуналка, Развлечения'; 
-let deposit = true; 
+let addExpenses = prompt('Перечислите возможные расходы за расчитываемый период через запятую'); 
+let deposit = confirm('Есть ли у вас депозит в банке?'); 
+let expenses1 = prompt('Введите обязательную статью расходов');
+let amount1 = Number(prompt('Во сколько вам это обойдется?'));
+let expenses2 = prompt('Введите обязательную статью расходов');
+let amount2 = Number(prompt('Во сколько вам это обойдется?'));
 let mission = 1400000; 
 let period = 12;
-let budgetDay;
-
+let budgetMonth = money - (amount1 + amount2);
+let budgetDay = budgetMonth / 30;
 
 
 console.log(typeof money, typeof income, typeof deposit);
@@ -14,6 +18,31 @@ console.log('Период равен ' + period + ' месяцев');
 console.log('Цель заработать ' + mission + ' рублей');
 console.log(addExpenses.toLowerCase());
 console.log(addExpenses.split(', '));
+console.log('Бюджет на месяц ' + Math.ceil(budgetMonth));
+console.log('Цель будет достигнута за: ' + mission/budgetMonth + ' месяцев');
+console.log('Бюджет на день ' + Math.floor(budgetDay) + ' рублей');
 
-budgetDay = money / 30;
-console.log(budgetDay);
+/* if (budgetDay >= 1200) {
+    console.log('У вас высокий уровень дохода');
+}else if (budgetDay > 600) {
+    console.log('У вас средний уровень дохода');
+}else if (budgetDay <= 600 || budgetDay > 1) {
+    console.log('К сожалению уровень дохода ниже среднего');
+}else if (budgetDay === 0 || budgetDay < 0) {
+    console.log('Что-то пошло не так');
+}   */
+
+if (budgetDay >= 1200) {
+    console.log('У вас высокий уровень дохода');
+}
+
+if (budgetDay > 600) {
+    console.log('У вас средний уровень дохода');
+}
+
+if (budgetDay <= 600) {
+    console.log('К сожалению уровень дохода ниже среднего');
+}
+if (budgetDay < 0) {
+    console.log('Что-то пошло не так');
+}
