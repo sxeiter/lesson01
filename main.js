69caec6885a85cf87f9893e46f24da72d30e2b52
Pavel Baksheev <sxeiter@mail.ru>
2020-06-10 02:00:24 +0500

    начал делать дз №4

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+'use strict';
 let money = prompt('Ваш ежемесячный доход?', 4000); 
 let income = 'продаю мёд'; 
 let addExpenses = prompt('Перечислите возможные расходы за расчитываемый период через запятую', 'еда, вода, жизнь'); 
@@ -11,8 +12,28 @@ let period = 12;
 let budgetMonth = money - (amount1 + amount2);
 let budgetDay = budgetMonth / 30;
 
+let showTypeOf = function(data) {
+    console.log(data, typeof(data));
+};
 
-console.log(typeof money, typeof income, typeof deposit);
+showTypeOf(money);
+showTypeOf(income);
+showTypeOf(deposit);
+
+let getExpensesMonth = function() {
+
+};
+let getAccumulatedMonth = function() {
+
+};
+let accumulatedMonth = getAccumulatedMonth() {
+
+};
+let getTargetMonth = function() {
+    
+};
+
+
 console.log(addExpenses.length);
 console.log('Период равен ' + period + ' месяцев');
 console.log('Цель заработать ' + mission + ' рублей');
@@ -22,14 +43,16 @@ console.log('Бюджет на месяц ' + budgetMonth);
 console.log('Цель будет достигнута за: ' + Math.ceil(mission/budgetMonth) + ' месяцев');
 console.log('Бюджет на день ' + Math.floor(budgetDay) + ' рублей');
 
-console.log(typeof budgetDay);
+let getStatusIncome = function(){
+    if (budgetDay >= 1200) {
+        return ('У вас высокий уровень дохода');
+    }else if (budgetDay > 600) {
+        return ('У вас средний уровень дохода');
+    }else if (budgetDay <= 600 && budgetDay > 0) {
+        return ('К сожалению уровень дохода ниже среднего');
+    }else if (budgetDay === 0 || budgetDay < 0) {
+        return ('Что-то пошло не так');
+    }
+};
+console.log(getStatusIncome());
 
-if (budgetDay >= 1200) {
-    console.log('У вас высокий уровень дохода');
-}else if (budgetDay > 600) {
-    console.log('У вас средний уровень дохода');
-}else if (budgetDay <= 600 && budgetDay > 0) {
-    console.log('К сожалению уровень дохода ниже среднего');
-}else if (budgetDay === 0 || budgetDay < 0) {
-    console.log('Что-то пошло не так');
-}
